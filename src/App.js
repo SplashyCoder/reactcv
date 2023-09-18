24ee32202c14860c9a14d9524589a6b074df9791
SplashyCoder <pachecomorahectordavid@gmail.com>
2023-09-18 14:55:56 -0500

    every thing ready

--- src/App.js
+++ src/App.js
@@ -12,137 +12,158 @@ return (
         description= 'Profile img'
         />
     </header>
-    <TextContaier class='namesTitle'>
+    <TextContaier className='namesTitle'>
         <div className="namesContainer">
             <h1 className="title title1">DAVID PACHECO</h1>
             <h1 className="title title2">FRONTEND DEVELOPER</h1>
         </div>
     </TextContaier>
-    <TextContaier class='profile'>
-    <div class="personalProfile">
-            <div class="personalProfileTitle">
-                <h1 class="subTitle">ABOUT</h1>
+    <TextContaier className='profile'>
+    <div className="personalProfile">
+            <div className="personalProfileTitle">
+                <h1 className="subTitle">ABOUT</h1>
             </div>
-            <p class="personalProfileDescription text">I’m a pacient person, with good soft and technical skills, futher I hold up in touch with the tools and skills in the development vanguard. I have experience in personal and academic projects in Frontend and Backend roles. Actually work in Linux platforms, however, I unwrap easily in Windows and Mac enviroments.  </p>
+            <p className="personalProfileDescription text">I’m a pacient person, with good soft and technical skills, futher I hold up in touch with the tools and skills in the development vanguard. I have experience in personal and academic projects in Frontend and Backend roles. Actually work in Linux platforms, however, I unwrap easily in Windows and Mac enviroments.  </p>
         </div>
     </TextContaier>
-    <TextContaier class='profile'>
-        <div class="personalProfile">
-            <div class="personalProfileTitle">
-                <h1 class="subTitle">TECH STACK</h1>
+    <TextContaier className='profile'>
+        <div className="personalProfile">
+            <div className="personalProfileTitle">
+                <h1 className="subTitle">TECH STACK</h1>
             </div>
-                <ul class="techStack" >
-                    <li class="randomItem1 text">Python 3.10</li>
-                    <li class="randomItem2 text">Random library 9.6</li>
+                <ul className="techStack" >
+                    <li className="passwordItem1 text">JavaScript</li>
+                    <li className="passwordItem2 text">HTML 5</li>
+                    <li className="passwordItem3 text">CSS 3</li>
+                    <li className="passwordItem1 text">React</li>
+                    <li className="passwordItem2 text">Python</li>
+                    <li className="passwordItem3 text">SQL</li>
+                    <li className="passwordItem1 text">Firebase</li>
+                    <li className="passwordItem2 text">Docker</li>
+                    <li className="passwordItem3 text">Bootstrap</li>
                 </ul>        
         </div>
     </TextContaier>
     
     <TextContaier>
-    <div class="education">
-            <h1 class="educationTitle subTitle">EDUCATION</h1>
-            <div class="university">
-                <h2 class="universityTitle subTitleH2">UNIVERSITY</h2>
-                <h3 class="universityName subTitleH3">UNIVERSIDAD DISTRITAL FRANCISCO JOSE DE CALDAS</h3>
-                <p class="universityCarrer text">Software Engineering (2018 - Today)</p>
+    <div className="education">
+            <h1 className="educationTitle subTitle">EDUCATION</h1>
+            <div className="university">
+                <h2 className="universityTitle subTitleH2">UNIVERSITY</h2>
+                <h3 className="universityName subTitleH3">UNIVERSIDAD DISTRITAL FRANCISCO JOSE DE CALDAS</h3>
+                <p className="universityCarrer text">Software Engineering (2018 - Today)</p>
             </div>
-            <div class="certifications">
-                <h2 class="certificationsTitle subTile">CERTIFICATIONS</h2>
-                <p class="certificationsText">
-                    <a class="certificationsLink text" href="https://ed.team/@davidpacheco/curso/python" target="_blank">Python-ED TEAM </a>| 
-                    <a class="certificationsLink text" href="https://ed.team/@davidpacheco/curso/sql" target="_blank">SQL Data Bases-ED TEAM </a>| 
-                    <a class="certificationsLink text" href="https://ed.team/@davidpacheco/curso/api-rest" target="_blank">Api Rest-ED TEAM </a>|
-                    <a class="certificationsLink text" href="https://ed.team/@davidpacheco/curso/api-rest" target="_blank">JavaScritp Oop-ED TEAM </a>|
-                    <a class="certificationsLink text" href="https://ed.team/@davidpacheco/curso/html" target="_blank">HTML-ED TEAM </a>|
-                    <a class="certificationsLink text" href="https://app.ed.team/u/davidpacheco/curso/css" target="_blank">CSS-ED TEAM </a>|
-                    <a class="certificationsLink text" href="https://app.ed.team/u/davidpacheco/curso/react-2021" target="_blank">React-ED TEAM </a>| 
-                    <a class="certificationsLink text" href="https://ed.team/@davidpacheco/curso/bootstrap" target="_blank">Boostrap 5-ED TEAM </a>|  
-                    <a class="certificationsLink text" href="https://ed.team/@davidpacheco/curso/git" target="_blank">Git-ED TEAM </a>
+            <div className="certifications">
+                <h2 className="certificationsTitle ">CERTIFICATIONS</h2>
+                <p className="certificationsText">
+                    <a className="certificationsLink text" href="https://ed.team/@davidpacheco/curso/python" target="_blank">Python-ED TEAM </a>| 
+                    <a className="certificationsLink text" href="https://ed.team/@davidpacheco/curso/sql" target="_blank">SQL Data Bases-ED TEAM </a>| 
+                    <a className="certificationsLink text" href="https://ed.team/@davidpacheco/curso/api-rest" target="_blank">Api Rest-ED TEAM </a>|
+                    <a className="certificationsLink text" href="https://ed.team/@davidpacheco/curso/api-rest" target="_blank">JavaScritp Oop-ED TEAM </a>|
+                    <a className="certificationsLink text" href="https://ed.team/@davidpacheco/curso/html" target="_blank">HTML-ED TEAM </a>|
+                    <a className="certificationsLink text" href="https://app.ed.team/u/davidpacheco/curso/css" target="_blank">CSS-ED TEAM </a>|
+                    <a className="certificationsLink text" href="https://app.ed.team/u/davidpacheco/curso/react-2021" target="_blank">React-ED TEAM </a>| 
+                    <a className="certificationsLink text" href="https://ed.team/@davidpacheco/curso/bootstrap" target="_blank">Boostrap 5-ED TEAM </a>|  
+                    <a className="certificationsLink text" href="https://ed.team/@davidpacheco/curso/git" target="_blank">Git-ED TEAM </a>
                 </p>
             </div>
         </div>
     </TextContaier>
     <TextContaier>
-        <div class="projects">
-                <h1 class="subTitle">PROJECTS</h1>
-                <div class="password">
-                    <div class="passwordTitle "><h2 class="subTileH2">PASSWORD MANAGER</h2></div>
-                    <div class="passwordDescription descript text">Project oriented to make a password manager by terminal. Using Python conected to a data base in a Docker container.</div>
-                    <div class="passwordStack">
-                        <ul class="techStack">
-                            <h4 class="text">Technologies</h4>
-                            <li class="passwordItem1 text">JavaScript</li>
-                            <li class="passwordItem2 text">HTML 5</li>
-                            <li class="passwordItem3 text">CSS 3</li>
-                            <li class="passwordItem1 text">React</li>
-                            <li class="passwordItem2 text">Python</li>
-                            <li class="passwordItem3 text">SQL</li>
-                            <li class="passwordItem1 text">Firebase</li>
-                            <li class="passwordItem2 text">Docker</li>
-                            <li class="passwordItem3 text">Bootstrap</li>
+        <div className="projects">
+        
+                <h1 className="subTitle">PROJECTS</h1>
+
+                <div className="password">
+                    <div className="passwordTitle "><h2 className="subTileH2">SOLUCIONES HORIZONTALES BOLIVAR</h2></div>
+                    <div className="passwordDescription descript text">Web page for a management company. Wich show services, advantages and its able to conect it with new coustomers.</div>
+                    <div className="passwordStack">
+                        <h4 className="text">Technologies</h4>
+                        <ul className="techStack">
+                            <li className="passwordItem1 text">HTML 5</li>
+                            <li className="passwordItem2 text">CSS 3</li>
+                            <li className="passwordItem3 text">JavaScritp</li>
+                            <li className="passwordItem3 text">Firebase(Hosting)</li>
                         </ul>
                     </div>
-                    <div class="passwordLink text">GITHUB:<a class="gitHub text" href="https://github.com/SplashyCoder/PASSWORD_MANAGER">    https://github.com/SplashyCoder/PASSWORD_MANAGER</a></div>
+                    <div className="webs">
+                        <div className="passwordLink text"><a className="gitHub text" href="https://github.com/SplashyCoder/SHB">GITHUB </a><a className="gitHub text" href="https://splashycoder.github.io/SHB/">| WEB</a></div>
+                    </div>
                 </div>
-                <div class="random">
-                    <div class="randomTitle "><h2 class="subtitleH2">RANDOM ADDRESS GENERATOR</h2></div>
-                    <div class="randomdDescription descript text">App thought to can do a kind of random tourism. Generating an address to can start descovering a city since that point.</div>
-                    <div class="randomStack">
-                        <ul class="techStack" >
-                            <h4 class="text">Technologies</h4>
-                            <li class="randomItem1 text">Python 3.10</li>
-                            <li class="randomItem2 text">Random library 9.6</li>
+
+                <div className="password">
+                    <div className="passwordTitle "><h2 className="subTileH2">EDIFICIO BANCO GANADERO</h2></div>
+                    <div className="passwordDescription descript text">Web page for a business building. Wich show spaces for rent, services and its able to conect it with new coustomers.</div>
+                    <div className="passwordStack">
+                        <h4 className="text">Technologies</h4>
+                        <ul className="techStack">
+                            <li className="passwordItem1 text">HTML 5</li>
+                            <li className="passwordItem2 text">CSS 3</li>
+                            <li className="passwordItem3 text">JavaScritp</li>
+                            <li className="passwordItem3 text">Firebase(Hosting)</li>
                         </ul>
                     </div>
-                    <div class="randomLink text">GITHUB:<a class="gitHub text" href="https://github.com/SplashyCoder/Random-Address-Genterator">   https://github.com/SplashyCoder/Random-Address-Genterator</a></div>
-                <div class="website">
-                    <div class="websiteTitle"><h2 class="subTileH2">PERSONAL WEBSITE</h2></div>
-                    <div class="websiteDescription descript text">Portfolio to can show my technical skills and future projects.</div>
-                    <div class="websiteStack">
-                        <ul class="techStack">
-                            <h4 class="text">Technologies</h4>
-                            <li class="websiteItem1 text">React 16</li>
-                            <li class="websiteItem2 text">HTML 5</li>
-                            <li class="websiteItem3 text">CSS 3</li>
-                            <li class="websiteItem3 text">Node js 18.4</li>
+                    <div className="webs">
+                        <div className="passwordLink text"><a className="gitHub text" href="https://github.com/SplashyCoder/BANK_">GITHUB </a><a className="gitHub text" href="https://splashycoder.github.io/BANK_">| WEB</a></div>
+                    </div>
+                </div>
+
+                <div className="random">
+                    <div className="randomTitle "><h2 className="subtitleH2">RANDOM ADDRESS GENERATOR WEB</h2></div>
+                    <div className="randomdDescription descript text">It's a personal project focused on the tourism in Bogota. The principal concept it's to explore the city from a random point and discover the normal life in it.</div>
+                    <div className="randomStack">
+                    <h4 className="text">Technologies</h4>
+                        <ul className="techStack" >
+                            <li className="passwordItem1 text">HTML 5</li>
+                            <li className="passwordItem2 text">CSS 3</li>
+                            <li className="passwordItem3 text">JavaScritp</li>
+                            <li className="passwordItem3 text">Github pages(Hosting)</li>
                         </ul>
                     </div>
-                    <div class="websiteLink text">GITHUB:<a class="gitHub text" href="https://splashycoder.github.io/splashyCoderPortfolio/">  https://splashycoder.github.io/splashyCoderPortfolio/ </a></div>
+                    <div className="webs">
+                        <div className="passwordLink text"><a className="gitHub text" href="https://github.com/SplashyCoder/RANDOMWEB">GITHUB </a><a className="gitHub text" href="https://splashycoder.github.io/RANDOMWEB">| WEB</a></div>
+                    </div>
+                </div>
+
+                <div className="website">
+                    <div className="websiteTitle"><h2 className="subTileH2">PERSONAL WEBSITE</h2></div>
+                    <div className="websiteDescription descript text">Portfolio to can show my technical skills and future projects.</div>
+                    <div className="websiteStack">
+
+<h4 className="text">Technologies</h4>                        <ul className="techStack">
+                            <li className="websiteItem1 text">React 16</li>
+                            <li className="websiteItem2 text">HTML 5</li>
+                            <li className="websiteItem3 text">CSS 3</li>
+                            <li className="websiteItem3 text">Node js 18.4</li>
+                            <li className="passwordItem3 text">Github pages(Hosting)</li>
+                        </ul>
+                    </div>
+                    <div className="webs">
+                        <div className="passwordLink text"><a className="gitHub text" href="https://github.com/SplashyCoder/splashyCoderPortfolio">GITHUB </a><a className="gitHub text" href="https://splashycoder.github.io/splashyCoderPortfolio">| WEB</a></div>         
+                    </div>
                 </div>  
-                <div class="cv">
-                    <div class="cvITitle"><h2 class="subTileH2">CV</h2></div>
-                    <div class="cvDescription descript text">This CV made completly by me using just HTML and CSS.</div>
-                    <div class="cvStack">
-                        <ul class="techStack">
-                            <h4 class="text">Technologies</h4>
-                            <li class="cvItem2 text">HTML 5</li>
-                            <li class="cvItem3 text">CSS 3</li>
-                        </ul>
-                    </div>
-                    <div class="cvLink text">GITHUB:<a class="gitHub text" href="https://github.com/SplashyCoder/CV">  https://github.com/SplashyCoder/CV</a></div>
-                </div>
-                </div>
-    </div>
-    </TextContaier>
-    <footer class="footer">
-        <div class="footerTitle">
-            <h1 class="subTile">CONTACT</h1>
+
         </div>
-        <div class="conect">
-            <div class="linkedin conectPart">
-                <a class = "conection text" href="https://www.linkedin.com/in/david-pacheco-166623227/">
+    </TextContaier>
+    <footer className="footer">
+        <div className="footerTitle">
+            <h1 className="subTile">CONTACT</h1>
+        </div>
+        <div className="conect">
+            <div className="linkedin conectPart">
+                <a className = "conection text" href="https://www.linkedin.com/in/david-pacheco-166623227/">
                     <img src="https://splashycoder.github.io/CV/media/icons8-linkedin.svg" alt=""/>
                     <p className='link'>https://www.linkedin.com/in/david-pacheco-166623227/</p> 
                 </a>
             </div>
-            <div class="mail conectPart">
-                <div class = "conection text" href="">
+            <div className="mail conectPart">
+                <div className = "conection text" href="">
                     <img className='mailImg' src="https://splashycoder.github.io/CV/media/mail-2569.svg" alt=""/>
                     <p className='link'>pachecomorahectordavid@gmail.com</p>
                 </div>
             </div>
-            <div class="github conectPart">
-                <a class = "conection text" href="https://github.com/SplashyCoder">
+            <div className="github conectPart">
+                <a className = "conection text" href="https://github.com/SplashyCoder">
                     <img src="https://splashycoder.github.io/CV/media/icons8-github.svg" alt=""/>
                     <p className='link'>https://github.com/SplashyCoder</p>
                 </a>
