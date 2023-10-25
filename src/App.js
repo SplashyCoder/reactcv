47451951c1a3209a41679c3427fbb0faaf19f414
SplashyCoder <pachecomorahectordavid@gmail.com>
2023-10-25 16:13:42 -0500

    actualization

--- src/App.js
+++ src/App.js
@@ -23,50 +23,7 @@ return (
             <div className="personalProfileTitle">
                 <h1 className="subTitle">ABOUT</h1>
             </div>
-            <p className="personalProfileDescription text">I’m a pacient person, with good soft and technical skills, futher I hold up in touch with the tools and skills in the development vanguard. I have experience in personal and academic projects in Frontend and Backend roles. Actually work in Linux platforms, however, I unwrap easily in Windows and Mac enviroments.  </p>
-        </div>
-    </TextContaier>
-    <TextContaier className='profile'>
-        <div className="personalProfile">
-            <div className="personalProfileTitle">
-                <h1 className="subTitle">TECH STACK</h1>
-            </div>
-                <ul className="techStack" >
-                    <li className="passwordItem1 text">JavaScript</li>
-                    <li className="passwordItem2 text">HTML 5</li>
-                    <li className="passwordItem3 text">CSS 3</li>
-                    <li className="passwordItem1 text">React</li>
-                    <li className="passwordItem2 text">Python</li>
-                    <li className="passwordItem3 text">SQL</li>
-                    <li className="passwordItem1 text">Firebase</li>
-                    <li className="passwordItem2 text">Docker</li>
-                    <li className="passwordItem3 text">Bootstrap</li>
-                </ul>        
-        </div>
-    </TextContaier>
-    
-    <TextContaier>
-    <div className="education">
-            <h1 className="educationTitle subTitle">EDUCATION</h1>
-            <div className="university">
-                <h2 className="universityTitle subTitleH2">UNIVERSITY</h2>
-                <h3 className="universityName subTitleH3">UNIVERSIDAD DISTRITAL FRANCISCO JOSE DE CALDAS</h3>
-                <p className="universityCarrer text">Software Engineering (2018 - Today)</p>
-            </div>
-            <div className="certifications">
-                <h2 className="certificationsTitle ">CERTIFICATIONS</h2>
-                <p className="certificationsText">
-                    <a className="certificationsLink text" href="https://ed.team/@davidpacheco/curso/python" target="_blank">Python-ED TEAM </a>| 
-                    <a className="certificationsLink text" href="https://ed.team/@davidpacheco/curso/sql" target="_blank">SQL Data Bases-ED TEAM </a>| 
-                    <a className="certificationsLink text" href="https://ed.team/@davidpacheco/curso/api-rest" target="_blank">Api Rest-ED TEAM </a>|
-                    <a className="certificationsLink text" href="https://ed.team/@davidpacheco/curso/api-rest" target="_blank">JavaScritp Oop-ED TEAM </a>|
-                    <a className="certificationsLink text" href="https://ed.team/@davidpacheco/curso/html" target="_blank">HTML-ED TEAM </a>|
-                    <a className="certificationsLink text" href="https://app.ed.team/u/davidpacheco/curso/css" target="_blank">CSS-ED TEAM </a>|
-                    <a className="certificationsLink text" href="https://app.ed.team/u/davidpacheco/curso/react-2021" target="_blank">React-ED TEAM </a>| 
-                    <a className="certificationsLink text" href="https://ed.team/@davidpacheco/curso/bootstrap" target="_blank">Boostrap 5-ED TEAM </a>|  
-                    <a className="certificationsLink text" href="https://ed.team/@davidpacheco/curso/git" target="_blank">Git-ED TEAM </a>
-                </p>
-            </div>
+            <p className="personalProfileDescription text">I’m a pacient person, with good soft and technical skills, futher I hold up in touch with the tools and practices in the development vanguard. I have experience in personal and freelance projects in Frontend and Backend roles. Actually work in Linux platforms, however, I unwrap easily in Windows and Mac enviroments.  </p>
         </div>
     </TextContaier>
     <TextContaier>
@@ -76,14 +33,22 @@ return (
 
                 <div className="password">
                     <div className="passwordTitle "><h2 className="subTileH2">SOLUCIONES HORIZONTALES BOLIVAR</h2></div>
-                    <div className="passwordDescription descript text">Web page for a management company. Wich show services, advantages and its able to conect it with new coustomers.</div>
+                    <div className="passwordDescription descript text">
+                    Developed and launched a management company website, significantly enhancing the firm's online presence. This led to increased client engagement and streamlining operations. My role showcased my ability to create impactful digital solutions using the best coding practices and drive the company's modernization.
+                    <br />
+                    <br />
+                    GOALS:
+                    <br />
+                    The new clients for the company increased in 23%.
+                    </div>
                     <div className="passwordStack">
-                        <h4 className="text">Technologies</h4>
+                        <h4 className="text">TECHSTACK</h4>
                         <ul className="techStack">
                             <li className="passwordItem1 text">HTML 5</li>
                             <li className="passwordItem2 text">CSS 3</li>
                             <li className="passwordItem3 text">JavaScritp</li>
-                            <li className="passwordItem3 text">Firebase(Hosting)</li>
+                            <li className="passwordItem3 text">Formspree</li>
+                            <li className="passwordItem3 text">Firebase(Hosting, Auth)</li>
                         </ul>
                     </div>
                     <div className="webs">
@@ -93,14 +58,23 @@ return (
 
                 <div className="password">
                     <div className="passwordTitle "><h2 className="subTileH2">EDIFICIO BANCO GANADERO</h2></div>
-                    <div className="passwordDescription descript text">Web page for a business building. Wich show spaces for rent, services and its able to conect it with new coustomers.</div>
+                    <div className="passwordDescription descript text">
+                    Designed and launched a website for a commercial building , which played a pivotal role in promoting the offerings. The website effectively showcased the building features and benefits, leading to increased inquiries and stakeholder engagement. This endeavor highlighted my capacity to create compelling online platforms that drive interest and investment in real estate ventures.
+                    <br />
+                    <br />
+                    GOALS:
+                    <br />
+                    The spaces offer by the building's management are now ocupped in a 100%.
+                    The posible clients has increased in 43%.
+                    </div>
                     <div className="passwordStack">
-                        <h4 className="text">Technologies</h4>
+                        <h4 className="text">TECHSTACK</h4>
                         <ul className="techStack">
                             <li className="passwordItem1 text">HTML 5</li>
                             <li className="passwordItem2 text">CSS 3</li>
                             <li className="passwordItem3 text">JavaScritp</li>
-                            <li className="passwordItem3 text">Firebase(Hosting)</li>
+                            <li className="passwordItem3 text">Formspree</li>
+                            <li className="passwordItem3 text">Firebase(Hosting, Auth)</li>
                         </ul>
                     </div>
                     <div className="webs">
@@ -109,10 +83,11 @@ return (
                 </div>
 
                 <div className="random">
-                    <div className="randomTitle "><h2 className="subtitleH2">RANDOM ADDRESS GENERATOR WEB</h2></div>
-                    <div className="randomdDescription descript text">It's a personal project focused on the tourism in Bogota. The principal concept it's to explore the city from a random point and discover the normal life in it.</div>
+                    <div className="randomTitle "><h2 className="subtitleH2">RANDOM ADDRESS GENERATOR</h2></div>
+                    <div className="randomdDescription descript text">
+                    Conceived and brought to life a personal tourism project centered around the vibrant city of Bogota. The core idea revolves around exploring the city from random starting points, uncovering the authentic, everyday life of its diverse neighborhoods. This project celebrates my passion for sharing the genuine cultural experiences of Bogota with visitors and locals alike, offering a unique perspective on the city's hidden gems.</div>
                     <div className="randomStack">
-                    <h4 className="text">Technologies</h4>
+                    <h4 className="text">TECHSTACK</h4>
                         <ul className="techStack" >
                             <li className="passwordItem1 text">HTML 5</li>
                             <li className="passwordItem2 text">CSS 3</li>
@@ -126,13 +101,15 @@ return (
                 </div>
 
                 <div className="website">
-                    <div className="websiteTitle"><h2 className="subTileH2">PERSONAL WEBSITE</h2></div>
-                    <div className="websiteDescription descript text">Portfolio to can show my technical skills and future projects.</div>
+                    <div className="websiteTitle"><h2 className="subTileH2">PORTFOLIO</h2></div>
+                    <div className="websiteDescription descript text">
+                    As a frontend developer, I've meticulously crafted my personal portfolio to showcase a collection of web projects that exemplify my expertise. Through a combination of intuitive design and responsive user interfaces, I've created web solutions that seamlessly connect form and function. My portfolio not only serves as a testament to my skills but also as a living canvas where I bring ideas to life through code, demonstrating my commitment to the world of web development.
+                    </div>
                     <div className="websiteStack">
 
-<h4 className="text">Technologies</h4>                        <ul className="techStack">
+                        <h4 className="text">TECHSTACK</h4>                        
+                        <ul className="techStack">
                             <li className="websiteItem1 text">React 16</li>
-                            <li className="websiteItem2 text">HTML 5</li>
                             <li className="websiteItem3 text">CSS 3</li>
                             <li className="websiteItem3 text">Node js 18.4</li>
                             <li className="passwordItem3 text">Github pages(Hosting)</li>
@@ -142,7 +119,30 @@ return (
                         <div className="passwordLink text"><a className="gitHub text" href="https://github.com/SplashyCoder/splashyCoderPortfolio">GITHUB </a><a className="gitHub text" href="https://splashycoder.github.io/splashyCoderPortfolio">| WEB</a></div>         
                     </div>
                 </div>  
-
+        </div>
+    </TextContaier>
+    <TextContaier>
+    <div className="education">
+            <h1 className="educationTitle subTitle">EDUCATION</h1>
+            <div className="university">
+                <h2 className="universityTitle subTitleH2">UNIVERSITY</h2>
+                <h3 className="universityName subTitleH3">UNIVERSIDAD DISTRITAL FRANCISCO JOSE DE CALDAS</h3>
+                <p className="universityCarrer text">Software Engineering (2018 - Today)</p>
+            </div>
+            <div className="certifications">
+                <h2 className="certificationsTitle ">CERTIFICATIONS</h2>
+                <p className="certificationsText">
+                    <a className="certificationsLink text" href="https://ed.team/@davidpacheco/curso/python" target="_blank" rel="noreferrer" >Python-ED TEAM </a>| 
+                    <a className="certificationsLink text" href="https://ed.team/@davidpacheco/curso/sql" target="_blank" rel="noreferrer" >SQL Data Bases-ED TEAM </a>| 
+                    <a className="certificationsLink text" href="https://ed.team/@davidpacheco/curso/api-rest" target="_blank" rel="noreferrer" >Api Rest-ED TEAM </a>|
+                    <a className="certificationsLink text" href="https://ed.team/@davidpacheco/curso/api-rest" target="_blank" rel="noreferrer" >JavaScritp Oop-ED TEAM </a>|
+                    <a className="certificationsLink text" href="https://ed.team/@davidpacheco/curso/html" target="_blank" rel="noreferrer" >HTML-ED TEAM </a>|
+                    <a className="certificationsLink text" href="https://app.ed.team/u/davidpacheco/curso/css" target="_blank" rel="noreferrer" >CSS-ED TEAM </a>|
+                    <a className="certificationsLink text" href="https://app.ed.team/u/davidpacheco/curso/react-2021" target="_blank" rel="noreferrer" >React-ED TEAM </a>| 
+                    <a className="certificationsLink text" href="https://ed.team/@davidpacheco/curso/bootstrap" target="_blank" rel="noreferrer" >Boostrap 5-ED TEAM </a>|  
+                    <a className="certificationsLink text" href="https://ed.team/@davidpacheco/curso/git" target="_blank" rel="noreferrer" >Git-ED TEAM </a>
+                </p>
+            </div>
         </div>
     </TextContaier>
     <footer className="footer">
@@ -150,6 +150,12 @@ return (
             <h1 className="subTile">CONTACT</h1>
         </div>
         <div className="conect">
+            <div className="phone conectPart">
+                <a className = "conection text" href="https://wa.me/573108504710?text=Hello%20David%20I%20want%20to%20talk%20you%20about%20my%20project/company">
+                    <img src="https://splashycoder.github.io/CV/media/icons8-phone.svg" alt=""/>
+                    <p className='link'>+57 3108504710</p>
+                </a>
+            </div>
             <div className="linkedin conectPart">
                 <a className = "conection text" href="https://www.linkedin.com/in/david-pacheco-166623227/">
                     <img src="https://splashycoder.github.io/CV/media/icons8-linkedin.svg" alt=""/>
