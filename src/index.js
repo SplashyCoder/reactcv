5437b1889606c4de1cdb61eb08730f8bc086516a
splashycoder <splashprobes@gmail.com>
2025-10-04 13:53:37 -0500

    Modificacion para no traer imposrts innecesarios

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import AppSpanish from './AppSpanish';
 import AppTodo from './AppTodo';
-import AppTodoSpanish from './AppTodoSpanish';
 import reportWebVitals from './reportWebVitals';
 // import React, { useState, useEffect } from 'react';
 
